288fef2408e94ac7ddbad27bb4886d910d4a16d3
Ubuntu <natalia@maszyna.5ujhd5sz435upetibmsazyargc.ax.internal.cloudapp.net>
2020-03-25 18:09:29 +0000

    Dodano wyświetlanie listy zadań

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
+const store = require('../store')
+
 const router = require('koa-router')()
 
+
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
+  tasks = await store.listTasks()
+  await ctx.render('index', { tasks })
+})
+.get('/', async (ctx, next) => {
+  ctx.render('index', {
     title: 'Hello Koa 2!'
   })
 })
 
-router.get('/string', async (ctx, next) => {
+outer.get('/string', async (ctx, next) => {
   ctx.body = 'koa2 string'
 })
+
 
 router.get('/json', async (ctx, next) => {
   ctx.body = {
@@ -17,3 +25,4 @@ router.get('/json', async (ctx, next) => {
 })
 
 module.exports = router
+
--- store.js
+++ store.js
@@ -35,3 +35,22 @@ module.exports = {
   createTask
 }
 
+const listTasks = async () => (
+  new Promise((resolve, reject) => {
+    const query = new storage.TableQuery()
+      .select(['title'])
+      .where('PartitionKey eq ?', 'task')
+
+    service.queryEntities(table, query, null, (error, result, response) => {
+      !error ? resolve(result.entries.map((entry) => ({
+        title: entry.title._
+      }))) : reject()
+    })
+  })
+)
+module.exports = {
+  init,
+  createTask,
+  listTasks
+}
+
