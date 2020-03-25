9425872ce5106328e54b164e878764dd9678b07a
Ubuntu <natalia@maszyna.5ujhd5sz435upetibmsazyargc.ax.internal.cloudapp.net>
2020-03-25 17:26:23 +0000

    Dodano funkcję tworzenia zadań po raz 3

--- store.js
+++ store.js
@@ -1,4 +1,19 @@
+const storage = require('azure-storage')
+const service = storage.createTableService()
+const table = 'tasks'
 const uuid = require('uuid')
+
+const init = async () => (
+  new Promise((resolve, reject) => {
+    service.createTableIfNotExists(table, (error, result, response) => {
+      !error ? resolve() : reject()
+    })
+  })
+)
+
+module.exports = {
+  init
+}
 
 const createTask = async (title) => (
   new Promise((resolve, reject) => {
@@ -14,8 +29,9 @@ const createTask = async (title) => (
     })
   })
 )
+
 module.exports = {
   init,
   createTask
 }
- 
+
