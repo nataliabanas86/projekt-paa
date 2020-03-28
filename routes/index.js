21ce12207794056604df8f5d6ae5bd273eef3da9
Ubuntu <natalia@maszyna.5ujhd5sz435upetibmsazyargc.ax.internal.cloudapp.net>
2020-03-28 10:26:52 +0000

    Dodano wyświetlanie listy zadań

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
-const router = require('koa-router')()
 const store = require('../store')
+const router = require('koa-router')()
+
+
 router.get('/', async (ctx, next) => {
   tasks = await store.listTasks()
   await ctx.render('index', { tasks })
-})
+  })  
+
 router.get('/string', async (ctx, next) => {
   ctx.body = 'koa2 string'
 })
+
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
   }
 })
+
 module.exports = router
+
